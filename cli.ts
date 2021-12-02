07a056aafc561c219164f289141d80e426d196db
Tiberius Silivestru <tiberius.silivestru@gmail.com>
2021-12-01 22:15:23 -0600

    silence node warnings

--- cli.ts
+++ cli.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node --loader ts-node/esm --experimental-specifier-resolution=node
+#!/usr/bin/env node --loader ts-node/esm --experimental-specifier-resolution=node --no-warnings
 
 import { Command } from "commander";
 
