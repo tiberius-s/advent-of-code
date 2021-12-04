e72244a5962a1e2d6ba203806b60c4653bcd545e
Tiberius Silivestru <tiberius.silivestru@gmail.com>
2021-12-04 15:37:47 -0600

    a lil cleanup on day three

--- solutions/three/index.ts
+++ solutions/three/index.ts
@@ -10,7 +10,7 @@ export function three(): void {
   const readings = IO.parse<number[]>(filepath, toNumberArr);
 
   // PART 1
-  const binGamma = readings.reduce(countBitsByPosition).map(mapMostCommonVal).join("").toString();
+  const binGamma = readings.reduce(countBitsByPosition).map(mapMostCommonVal(readings.length)).join("").toString();
 
   const gamma = getGamma(binGamma);
   const epsilon = getEpsilon(gamma, binGamma.length);
@@ -26,79 +26,81 @@ export function three(): void {
   const answerTwo = ogRating * co2sRating;
 
   console.log(`Part 2 answer is ${answerTwo}\n`);
+}
 
-  // UTILS
-  function mapMostCommonVal(val: number): number {
-    return val > readings.length / 2 ? 1 : 0;
-  }
+// UTILS
+function mapMostCommonVal(arrLength: number): (x: number) => number {
+  return function (val: number): number {
+    return val > arrLength / 2 ? 1 : 0;
+  };
+}
 
-  function toNumberArr(arr: string[]) {
-    return arr.map((x) =>
-      x
-        .split("")
-        .filter((y) => y.length > 0)
-        .map((y) => parseInt(y, 10))
-    );
-  }
+function toNumberArr(arr: string[]) {
+  return arr.map((x) =>
+    x
+      .split("")
+      .filter((y) => y.length > 0)
+      .map((y) => parseInt(y, 10))
+  );
+}
 
-  function countBitsByPosition(prev: number[], curr: number[]): number[] {
-    return curr.map((val, idx) => val + prev[idx]);
-  }
+function countBitsByPosition(prev: number[], curr: number[]): number[] {
+  return curr.map((val, idx) => val + prev[idx]);
+}
 
-  function getGamma(val: string) {
-    return parseInt(val, 2);
-  }
+function getGamma(val: string) {
+  return parseInt(val, 2);
+}
 
-  function getEpsilon(val: number, binLength: number) {
-    return ~val & (Math.pow(2, binLength) - 1);
-  }
+function getEpsilon(val: number, binLength: number) {
+  return ~val & (Math.pow(2, binLength) - 1);
+}
 
-  function sumAtIndex(idx: number) {
-    return function (prev: number[], curr: number[]): number[] {
-      const result = curr.map((val) => val + prev[idx]);
-      return result;
-    };
-  }
+function sumAtIndex(idx: number): (prev: number[], curr: number[]) => number[] {
+  return function (prev: number[], curr: number[]): number[] {
+    const result = curr.map((val) => val + prev[idx]);
+    return result;
+  };
+}
 
-  function getLeastCommon(onesCount: number, arrLength: number) {
-    const zerosCount = arrLength - onesCount;
-    if (zerosCount > onesCount) {
-      return 1;
-    } else if (zerosCount < onesCount) {
-      return 0;
-    }
+function getLeastCommon(onesCount: number, arrLength: number) {
+  const zerosCount = arrLength - onesCount;
+  if (zerosCount > onesCount) {
+    return 1;
+  } else if (zerosCount < onesCount) {
     return 0;
   }
+  return 0;
+}
 
-  function getMostCommon(onesCount: number, arrLength: number) {
-    const zerosCount = arrLength - onesCount;
-    if (zerosCount < onesCount) {
-      return 1;
-    } else if (zerosCount > onesCount) {
-      return 0;
-    }
+function getMostCommon(onesCount: number, arrLength: number) {
+  const zerosCount = arrLength - onesCount;
+  if (zerosCount < onesCount) {
     return 1;
+  } else if (zerosCount > onesCount) {
+    return 0;
+  }
+  return 1;
+}
+
+function diagnosticFilter(arr: number[][], getFilterValue: Function) {
+  let result = arr;
+  let idx = 0;
+
+  while (result.length > 1) {
+    const total = <number>result.reduce(sumAtIndex(idx))[idx];
+    const filterVal = getFilterValue(total, result.length);
+    result = result.filter((x) => x[idx] == filterVal);
+    idx++;
   }
 
-  function diagnosticFilter(arr: number[][], getFilterValue: Function) {
-    let result = arr;
-    let idx = 0;
+  return parseInt(result[0].join(""), 2);
+}
 
-    while (result.length > 1) {
-      const total = <number>result.reduce(sumAtIndex(idx))[idx];
-      const filterVal = getFilterValue(total, result.length);
-      result = result.filter((x) => x[idx] == filterVal);
-      idx++;
-    }
+function getOxygenGeneratorRating(arr: number[][]) {
+  return diagnosticFilter(arr, getMostCommon);
+}
 
-    return parseInt(result[0].join(""), 2);
-  }
-
-  function getOxygenGeneratorRating(arr: number[][]) {
-    return diagnosticFilter(arr, getMostCommon);
-  }
-
-  function getCO2ScrubberRating(arr: number[][]) {
-    return diagnosticFilter(arr, getLeastCommon);
-  }
+function getCO2ScrubberRating(arr: number[][]) {
+  return diagnosticFilter(arr, getLeastCommon);
 }
