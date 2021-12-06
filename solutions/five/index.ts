97a5da6387ce014b6d8db68f3aef103187fcef98
Tiberius Silivestru <tiberius.silivestru@gmail.com>
2021-12-06 16:14:49 -0600

    finished day five

--- solutions/five/index.ts
+++ solutions/five/index.ts
@@ -1,36 +1,46 @@
+import { count } from "console";
+import { parse } from "path/posix";
 import { URL } from "url";
 import { IO } from "../utils/io";
 
-type Coordinates = [x: number, y: number];
-type Line = [source: Coordinates, destination: Coordinates];
+type Point = { x: number; y: number };
+type Line = [source: Point, destination: Point];
 
 export function five(): void {
-  console.log("\nDAY 5: https://adventofcode.com/2021/day/5\n");
+  console.log("\nDAY 5: https://adventofcode.com/2021/day/5");
 
-  const fileUrl = new URL("./test.txt", import.meta.url);
+  const fileUrl = new URL("./input.txt", import.meta.url);
 
   const input = IO.parse<Line>(fileUrl, toLineArr);
 
   // PART 1
   const inputNoDiag = filterOutDiagonals(input);
-  let diagramOne = createDiagram(input.length);
+  let diagramOne = createDiagram(1000);
 
   for (const line of inputNoDiag) {
     diagramOne = drawLine(line, diagramOne);
   }
+  const answerOne = computeAnswer(diagramOne);
+  console.log(`\nAnswer 1: ${answerOne}`);
 
-  printDiagram(diagramOne);
+  // PART 2
+  let diagramTwo = createDiagram(1000);
+
+  for (const line of input) {
+    diagramTwo = drawLine(line, diagramTwo);
+  }
+
+  const answerTwo = computeAnswer(diagramTwo);
+  console.log(`\nAnswer 2: ${answerTwo}`);
 }
 
 // UTILS
 function toLineArr(input: string[]): Line[] {
   return input.map(
-    (l) => <Line>l.split("->").map(
-        (pair) => <Coordinates>pair
-            .trim()
-            .split(",")
-            .map((v) => parseInt(v, 10))
-      )
+    (l) => <Line>l
+        .split("->")
+        .map((pair) => pair.trim().split(","))
+        .map((v) => ({ x: parseInt(v[0]), y: parseInt(v[1]) }))
   );
 }
 
@@ -46,14 +56,59 @@ function printDiagram(diagram: number[][]): void {
 }
 
 function filterOutDiagonals(arr: Line[]): Line[] {
-  return arr.filter((line) => line[0][0] == line[1][0] || line[0][1] == line[1][1]);
+  return arr.filter((line) => line[0].x == line[1].x || line[0].y == line[1].y);
 }
 
 function drawLine(line: Line, diagram: number[][]): number[][] {
   const [source, destination] = line;
-  diagram[source[0]][source[1]] += 1;
-  diagram[destination[0]][destination[1]] += 1;
+  const [diffX, diffY] = [Math.abs(source.x - destination.x), Math.abs(source.y - destination.y)];
+  const xInc = destination.x > source.x ? 1 : -1;
+  const yInc = destination.y > source.y ? 1 : -1;
+  const destPoint = { x: destination.x, y: destination.y };
+  markPoint(destPoint, diagram);
+
+  // draw diagonal
+  if (Math.abs(diffX) && Math.abs(diffY)) {
+    let { x: currX, y: currY } = source;
+    while (currX !== destination.x || currY !== destination.y) {
+      const point = { x: currX, y: currY };
+      markPoint(point, diagram);
+      currX += xInc;
+      currY += yInc;
+    }
+
+    return diagram;
+  }
+
+  // draw horizontal
+  if (Math.abs(diffX)) {
+    let { x: currX, y: currY } = source;
+    while (currX !== destination.x) {
+      const point = { x: currX, y: currY };
+      markPoint(point, diagram);
+      currX += xInc;
+    }
+    return diagram;
+  }
+
+  // draw vertical
+  if (Math.abs(diffY)) {
+    let { x: currX, y: currY } = source;
+    while (currY !== destination.y) {
+      const point = { x: currX, y: currY };
+      markPoint(point, diagram);
+      currY += yInc;
+    }
+    return diagram;
+  }
   return diagram;
 }
 
-five()
+function markPoint(point: Point, diagram: number[][]): number[][] {
+  diagram[point.y][point.x]++;
+  return diagram;
+}
+
+function computeAnswer(diagram: number[][]): number {
+  return diagram.map((line) => line.filter((x) => x > 1).length).reduce((x, y) => x + y);
+}
